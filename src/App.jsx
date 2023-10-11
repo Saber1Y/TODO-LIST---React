b638a7cbc88f69e44884437166deaa84a9ea334f
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-11 01:13:24 +0100

    delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,30 @@ export default function App() {
            false },
       ]
     })
+
+    setNewItem('')
   }
 
 
+    function toggleTodo(id, completed) {
+      setTodos(currentTodos => {
+        return currentTodos.map(todo => {
+           if (todo.id === id) {
+            return { ...todo, completed }
+           }
+
+           return todo
+        })
+      })
+    }
+     
+
+     function deleteTodo(id) {
+       setTodos(currentTodos => {
+        return currentTodos.filter(todo => todo.id === id)
+       })
+
+     }
   return (
     <>
    <form className="new-item-form" onSubmit={handleSubmit}>
@@ -38,10 +59,12 @@ export default function App() {
       return (
     <li key={todo.id}>
       <label>
-        <input type="checkbox" checked={todo.completed} />
+        <input type="checkbox" checked={todo.completed} onChange={(e) => {
+          toggleTodo(todo.id, e.target.checked);
+        }}/>
         {todo.title}
       </label>
-        <button className="btn btn-danger">Delete</button>
+        <button className="btn btn-danger" onClick={deleteTodo}>Delete</button>
     </li>
       )
     })}
