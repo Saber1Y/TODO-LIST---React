b45a52f56e0e76ba1b2730792b0b89a16968775b
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-11 13:07:56 +0100

    UPDATE: Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
-import React, { useState }  from "react";
-import '/App.scss'
+import React, { useState } from "react";
+import '/App.scss';
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
-
 export default function App() {
-
   const [todos, setTodos] = useState([]);
 
   function AddTodo(title) {
     setTodos(currentTodos => {
       return [
         ...currentTodos, 
-        { id: crypto.randomUUID(), title, completed:
-           false },
+        { id: crypto.randomUUID(), title, completed: false },
       ]
     })
   }
-    function toggleTodo(id, completed) {
-      setTodos(currentTodos => {
-        return currentTodos.map(todo => {
-           if (todo.id === id) {
-            return { ...todo, completed }
-           }
 
-           return todo
-        })
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
       })
-    }
-     
+    })
+  }
 
-     function deleteTodo(id) {
-       setTodos(currentTodos => {
-        return currentTodos.filter(todo => todo.id === id)
-       })
-     }
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
     <TodoForm mainTodo={AddTodo} />
-   <h1>Added Todo's: </h1>
-    <TodoList />
+    <h1>Added Todo's: </h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
    </>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 
-const TodoList = ({ todos,  }) => {
+export default function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
     {todos.length === 0 && <li>Please Add todos</li>}
     {todos.map(todo => {
       return (
-    <li key={todo.id}>
-      <label>
-        <input type="checkbox" checked={todo.completed} 
-        onChange={(e) => {toggleTodo(todo.id, e.target.
-            checked);
-        }}/>
-        {todo.title}
-      </label>
-        <button className="btn btn-danger" 
-         onClick={deleteTodo}>Delete</button> 
-    </li>
+        <li key={todo.id}>
+          <label>
+            <input 
+              type="checkbox" 
+              checked={todo.completed} 
+              onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+            />
+            {todo.title}
+          </label>
+          <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button> 
+        </li>
       )
     })}
    </ul>
   )
 }
-
-export default TodoList
